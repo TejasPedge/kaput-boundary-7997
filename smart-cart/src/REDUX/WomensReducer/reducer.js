13708461d75ea504ff8aff9030206ed25c3632e2
kulsoomrasheed <rasheedamaan111@gmail.com>
2023-03-30 22:59:42 +0530

    day2 completed fw21_0123

---
+++ smart-cart/src/REDUX/WomensReducer/reducer.js
@@ -0,0 +1,11 @@
+const initialState={
+isLoading:false,
+isError:false,
+products:[]
+}
+export const reducer=(state=initialState,{type,payload})=>{
+switch(type){
+
+    default:return state
+}
+}
--- smart-cart/src/REDUX/store.js
+++ smart-cart/src/REDUX/store.js
@@ -1,8 +1,8 @@
 import {legacy_createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import {reducer as mensReducer} from './Men\'s Reducer/reducer';
-
-const Root_Reducer = combineReducers({mensReducer});
+import {reducer as womensReducer} from "./WomensReducer/reducer"
+const Root_Reducer = combineReducers({mensReducer, womensReducer});
 const Middleware =  applyMiddleware(thunk);
 
 export const store = legacy_createStore(Root_Reducer,Middleware);
